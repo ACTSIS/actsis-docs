990c606ebfbd9c30e1d5c74946b27225b2fe468a
daniel.rondon <daniel.rondon@actsis.com>
2025-01-22 19:11:06 -0500

    feat: se implenta auth a la documentación en general

---
+++ website/src/AuthConfig.js
@@ -0,0 +1,53 @@
+import { LogLevel } from "@azure/msal-browser";
+
+/**
+ * Configuration object to be passed to MSAL instance on creation.
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
+ */
+
+export const msalConfig = {
+    auth: {
+        clientId: "aefd1102-5932-42c2-8385-6b1eb9937537",
+        authority: "https://login.microsoftonline.com/6020db66-8a41-417f-9c87-691ad3ee560f",
+        redirectUri: "http://localhost:3000",
+        postLogoutRedirectUri: '/',
+    },
+    cache: {
+        cacheLocation: "sessionStorage", // This configures where your cache will be stored
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback: (level, message, containsPii) => {
+                if (containsPii) {
+                    return;
+                }
+                switch (level) {
+                    case LogLevel.Error:
+                        console.error(message);
+                        return;
+                    case LogLevel.Info:
+                        console.info(message);
+                        return;
+                    case LogLevel.Verbose:
+                        console.debug(message);
+                        return;
+                    case LogLevel.Warning:
+                        console.warn(message);
+                        return;
+                    default:
+                        return;
+                }
+            }
+        }
+    }
+};
+
+/**
+ * Scopes you add here will be prompted for user consent during sign-in.
+ * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+ */
+export const loginRequest = {
+    scopes: ["User.Read"],
+};
